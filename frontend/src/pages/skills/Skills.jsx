4d01fe39ba564c2e4ccc3147b2ca69cd8c9f44a7
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-05-10 17:24:36 +0200

    Remove useless import

--- frontend/src/pages/skills/Skills.jsx
+++ frontend/src/pages/skills/Skills.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../../components/header/Header';
 import Starfield from '../../components/starfield/Starfield';
 import SkillCard from '../../components/cards/SkillCard';
-import TitleCard from '../../components/cards/TitleCard';
 import Footer from '../../components/footer/Footer';
 import { useLanguage } from '../../utils/language/LanguageContext';
 
