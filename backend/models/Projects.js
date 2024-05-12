bbea5827c2857a9a15c40aa465a4ee17540aac92
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-05-12 23:12:30 +0200

    Update model for projects table in db

--- backend/models/Projects.js
+++ backend/models/Projects.js
@@ -4,7 +4,11 @@ const projectsSchema = mongoose.Schema({
   nameKey: { type: String, required: true },
   skillsRequire: [ { type: String, required: true } ],
   imagePreviewPaths: { type: String, required: true },
-  imagePaths: [ { type: String } ],
+  imagePaths: {
+    screen1: { type: String, required: true },
+    screen2: { type: String, required: true },
+    screen3: { type: String, required: true },
+  },
   githubLink: { type: String },
 });
 
