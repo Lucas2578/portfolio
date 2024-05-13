4cdbaaf179eb1a2415cc46963a450d2d358d454b
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-05-13 03:28:45 +0200

    Add CV and link to linkedin in footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
         <footer className="footer">
             <div className="footer__desc">   
                 <div className="footer__desc__links">
-                    <a href="test" className="footer__desc__links--link"><i className="fa-brands fa-linkedin footer__desc__links--icon"></i></a>
-                    <a href="test" className="footer__desc__links--link"><i className="fa-solid fa-envelope footer__desc__links--icon"></i></a>
-                    <a href="test" className="footer__desc__links--link"><i className="fa-solid fa-file-lines footer__desc__links--icon"></i></a>
+                    <a href="https://www.linkedin.com/in/lucas-dunis-431b07308" target="_blank" rel="noopener noreferrer" className="footer__desc__links--link"><i className="fa-brands fa-linkedin footer__desc__links--icon"></i></a>
+                    <a href="mailto:lucasdunis@gmail.com" className="footer__desc__links--link"><i className="fa-solid fa-envelope footer__desc__links--icon"></i></a>
+                    <a href="assets/CV_Dunis_Lucas.pdf" target="_blank" rel="noopener noreferrer" className="footer__desc__links--link"><i className="fa-solid fa-file-lines footer__desc__links--icon"></i></a>
                 </div>
                 <div className="footer__desc__info">
                     <i className="fa-solid fa-copyright footer__desc__info--icon"></i><p>Portfolio Dunis Lucas 2024</p>
