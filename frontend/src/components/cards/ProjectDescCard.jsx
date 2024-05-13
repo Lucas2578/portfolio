5f7224420fd74e295eb06351eb187429f96f94ea
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-05-13 02:47:11 +0200

    Add condition for no showing github icon if githubLink is empty

--- frontend/src/components/cards/ProjectDescCard.jsx
+++ frontend/src/components/cards/ProjectDescCard.jsx
@@ -27,27 +27,31 @@ const ProjectDescCard = () => {
     const { handleMouseEnter, handleMouseLeave, isItemHovered } = useModal();
 
     return (
-      <div className="project__info__card">
-        <div className="project__info__card__header">
-          <i className="fa-solid fa-circle red"></i>
-          <i className="fa-solid fa-circle yellow"></i>
-          <i className="fa-solid fa-circle green"></i>
-        </div>
-        <div className="project__info__card__body">
-          <h2 className="project__info__card__body--title">{translateDescCardProjectTitle(projectNameKey, translations.project_page.project)}</h2>
-          <div className="spacer"></div>
-          <div className="project__info__card__body__desc">
-              <p>{translateDescCardProjectContent(projectNameKey, translations.project_page.project)}</p>
-          </div>
-          <div className="spacer"></div>
-          <div className="project__info__card__body__icon">
-          {/* Modal on hover github icon */}
-          <Modal project={project} modalClass="project__github" title={translations.project_page.project.modal.clickhere} isModalOpen={isItemHovered(`github_${project.nameKey}`)} />
-              <a href={project.githubLink} target="_blank" rel="noopener noreferrer" onMouseEnter={() => handleMouseEnter(`github_${project.nameKey}`)} onMouseLeave={handleMouseLeave}>
-                  <i className="fa-brands fa-github project__info__card__body__icon--img"></i>
-              </a>
-          </div>
-        </div>
+        <div className="project__info__card">
+            <div className="project__info__card__header">
+                <i className="fa-solid fa-circle red"></i>
+                <i className="fa-solid fa-circle yellow"></i>
+                <i className="fa-solid fa-circle green"></i>
+            </div>
+            <div className="project__info__card__body">
+                <h2 className="project__info__card__body--title">{translateDescCardProjectTitle(projectNameKey, translations.project_page.project)}</h2>
+                <div className="spacer"></div>
+                <div className="project__info__card__body__desc">
+                    <p>{translateDescCardProjectContent(projectNameKey, translations.project_page.project)}</p>
+                </div>
+                <div className="spacer"></div>
+                <div className="project__info__card__body__icon">
+                    {/* Modal on hover github icon */}
+                    {project.githubLink && (
+                            <>
+                                <Modal project={project} modalClass="project__github" title={translations.project_page.project.modal.clickhere} isModalOpen={isItemHovered(`github_${project.nameKey}`)} />
+                                <a href={project.githubLink} target="_blank" rel="noopener noreferrer" onMouseEnter={() => handleMouseEnter(`github_${project.nameKey}`)} onMouseLeave={handleMouseLeave}>
+                                    <i className="fa-brands fa-github project__info__card__body__icon--img"></i>
+                                </a>
+                            </>
+                        )}
+                </div>
+            </div>
       </div>
     );
 };
