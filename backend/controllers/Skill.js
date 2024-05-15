15e20a34a4e693fb01dc9bdd6600f548f9a374f5
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-05-15 14:14:27 +0200

    Add sort for skills

--- backend/controllers/Skill.js
+++ backend/controllers/Skill.js
@@ -3,12 +3,14 @@ const SkillOther = require('../models/SkillsOther');
 
 exports.getAllSkillsWeb = (req, res, next) => {
     SkillWeb.find()
+        .sort({ level: -1 })
         .then(skills => res.status(200).json(skills))
         .catch(error => res.status(404).json({ error }));
 }
 
 exports.getAllSkillsOther = (req, res, next) => {
     SkillOther.find()
+        .sort({ level: -1 })
         .then(skills => res.status(200).json(skills))
         .catch(error => res.status(404).json({ error }));
 }
