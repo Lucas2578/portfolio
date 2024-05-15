ad280ff651feef93ad06ad930d5611e12cd7e1eb
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-05-15 15:16:30 +0200

    Resize images in projects page for optimize performance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import "./styles/app.scss";
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/home/Home';
@@ -5,7 +6,6 @@ import Skills from './pages/skills/Skills';
 import Projects from './pages/projects/Projects';
 import Project from './pages/projects/Project';
 import { LanguageProvider } from './utils/language/LanguageContext';
-import "./styles/app.scss";
 
 const App = () => {
   
--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -14,11 +14,13 @@ function Footer() {
                     <a href="mailto:lucasdunis@gmail.com" className="footer__desc__links--link">
                         {/* This span is necessary for resolve wave errors with <a> tag missing text */}
                         <span className="footer__desc__links--invisible-text">Mail</span>
-                        <i className="fa-solid fa-envelope footer__desc__links--icon"></i></a>
+                        <i className="fa-solid fa-envelope footer__desc__links--icon"></i>
+                    </a>
                     <a href="assets/CV_Dunis_Lucas.pdf" target="_blank" rel="noopener noreferrer" className="footer__desc__links--link">
                         {/* This span is necessary for resolve wave errors with <a> tag missing text */}
                         <span className="footer__desc__links--invisible-text">CV</span>
-                        <i className="fa-solid fa-file-lines footer__desc__links--icon"></i></a>
+                        <i className="fa-solid fa-file-lines footer__desc__links--icon"></i>
+                    </a>
                 </div>
                 <div className="footer__desc__info">
                     <i className="fa-solid fa-copyright footer__desc__info--icon"></i><p>Portfolio Dunis Lucas 2024</p>
